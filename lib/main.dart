dae52d0dca756b9afa424af0ea5093c32da747a9
Abdo-Elsabagh <aelsabaggha2000@gmail.com>
2024-10-01 08:49:11 +0300

    "Extracted news API call into NewsService class and parsed response into ArticleModel list"

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,11 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app/services/news_service.dart';
 import 'package:news_app/splash_screen.dart';
 
 void main() {
-  getNews();
+  NewsService(Dio()).getNews();
   runApp(const MyApp());
-}
-
-final dio = Dio();
-
-void getNews() async {
-  final response = await dio.get(
-      'https://newsapi.org/v2/top-headlines?country=us&apiKey=b5dde141ef6842039e4e9b24759d3a68');
-  print(response);
 }
 
 class MyApp extends StatelessWidget {
--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:news_app/models/article_model.dart';
 
 class NewsService {
   final Dio dio;
@@ -6,8 +7,25 @@ class NewsService {
   NewsService(this.dio);
 
   void getNews() async {
-    final response = await dio.get(
+    var response = await dio.get(
         'https://newsapi.org/v2/top-headlines?country=us&apiKey=b5dde141ef6842039e4e9b24759d3a68');
-    print(response);
+
+    Map<String, dynamic> jsonData = response.data;
+    List<dynamic> articles = jsonData['articles'];
+    // for (var article in articles) {
+    //   print(article['author']);
+    // }
+
+    List<ArticleModel> articlesList = [];
+
+    for (var article in articles) {
+      ArticleModel articleModel = ArticleModel(
+        image: article['urlToImage'],
+        title: article['title'],
+        subTitle: article['description'],
+      );
+      articlesList.add(articleModel);
+    }
+    print(articlesList);
   }
 }
