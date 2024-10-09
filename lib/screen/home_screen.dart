55f5f70080b0a8b1938f389ec56e403942b9d680
Abdo-Elsabagh <aelsabaggha2000@gmail.com>
2024-10-09 20:33:26 +0300

    "Removed NewsListViewBuilder stateful widget and added NewsListViewBuilder import in HomeView"

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:news_app/models/article_model.dart';
 import 'package:news_app/services/news_service.dart';
 import 'package:news_app/widgets/categories_list_view.dart';
+import 'package:news_app/widgets/ews_list_view_builder.dart';
 import 'package:news_app/widgets/news_list_viem.dart';
 
 class HomeView extends StatelessWidget {
@@ -61,36 +62,4 @@ class HomeView extends StatelessWidget {
   }
 }
 
-class NewsListViewBuilder extends StatefulWidget {
-  const NewsListViewBuilder({
-    super.key,
-  });
 
-  @override
-  State<NewsListViewBuilder> createState() => _NewsListViewBuilderState();
-}
-
-class _NewsListViewBuilderState extends State<NewsListViewBuilder> {
-  @override
-  List<ArticleModel> articles = [];
-  bool isLoad = true;
-  @override
-  void initState() {
-    super.initState();
-    getGeneralNews();
-  }
-
-  Future<void> getGeneralNews() async {
-    articles = await NewsService(Dio()).getNews();
-    isLoad = false;
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return isLoad
-        ? const SliverToBoxAdapter(
-            child: Center(child: CircularProgressIndicator()))
-        : NewsListViem(articles: articles);
-  }
-}
---
+++ lib/widgets/ews_list_view_builder.dart
@@ -0,0 +1,39 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:news_app/models/article_model.dart';
+import 'package:news_app/services/news_service.dart';
+import 'package:news_app/widgets/news_list_viem.dart';
+
+class NewsListViewBuilder extends StatefulWidget {
+  const NewsListViewBuilder({
+    super.key,
+  });
+
+  @override
+  State<NewsListViewBuilder> createState() => _NewsListViewBuilderState();
+}
+
+class _NewsListViewBuilderState extends State<NewsListViewBuilder> {
+  @override
+  List<ArticleModel> articles = [];
+  bool isLoad = true;
+  @override
+  void initState() {
+    super.initState();
+    getGeneralNews();
+  }
+
+  Future<void> getGeneralNews() async {
+    articles = await NewsService(Dio()).getNews();
+    isLoad = false;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return isLoad
+        ? const SliverToBoxAdapter(
+            child: Center(child: CircularProgressIndicator()))
+        : NewsListViem(articles: articles);
+  }
+}
