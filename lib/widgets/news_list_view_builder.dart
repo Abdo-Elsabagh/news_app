89d049fd0fd595e40f005d32e87c833c1f5aba61
Abdo-Elsabagh <aelsabaggha2000@gmail.com>
2024-11-19 01:30:33 +0200

    "Converted NewsListViewBuilder from StatelessWidget to StatefulWidget, added initState method to fetch news data, and updated FutureBuilder to specify type parameter."

--- lib/widgets/news_list_view_builder.dart
+++ lib/widgets/news_list_view_builder.dart
@@ -1,11 +1,17 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app/models/article_model.dart';
 import 'package:news_app/services/news_service.dart';
 import 'package:news_app/widgets/news_list_viem.dart';
 
-class NewsListViewBuilder extends StatelessWidget {
+class NewsListViewBuilder extends StatefulWidget {
   const NewsListViewBuilder({super.key});
 
+  @override
+  State<NewsListViewBuilder> createState() => _NewsListViewBuilderState();
+}
+
+class _NewsListViewBuilderState extends State<NewsListViewBuilder> {
   @override
 
   // bool isLoad = true;
@@ -20,14 +26,21 @@ class NewsListViewBuilder extends StatelessWidget {
   //   isLoad = false;
   //   setState(() {});
   // }
+  var future;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    future = NewsService(Dio()).getNews();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: NewsService(Dio()).getNews(),
+    return FutureBuilder<List<ArticleModel>>(
+        future: future,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return NewsListViem(articles: snapshot.data!); 
+            return NewsListViem(articles: snapshot.data!);
           } else if (snapshot.hasError) {
             return const SliverToBoxAdapter(
               child: Text('oops there was an error try later'),
