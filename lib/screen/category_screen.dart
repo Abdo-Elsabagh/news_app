cf7d65c97a45d7fe959eaed522a0a97f18a8c3da
Abdo-Elsabagh <aelsabaggha2000@gmail.com>
2024-11-24 20:14:12 +0200

    "Updated AndroidManifest.xml label, replaced ic_launcher.png files, and modified CategoryCard widget to add tap functionality and navigation."

---
+++ lib/screen/category_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CategroyViem extends StatelessWidget {
+  const CategroyViem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SizedBox(),
+    );
+  }
+}
--- lib/widgets/category_card.dart
+++ lib/widgets/category_card.dart
@@ -1,27 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/models/category_model.dart';
+import 'package:news_app/screen/category_screen.dart';
 
 class CategoryCaed extends StatelessWidget {
   const CategoryCaed({super.key, required this.category});
   final CategoryModel category;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 85,
-      width: 165,
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        image: DecorationImage(
-            image: AssetImage(category.image), fit: BoxFit.fill),
-        color: Colors.brown,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Text(
-        category.categoryName,
-        style: const TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-          fontSize: 20,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) {
+            return const CategroyViem();
+          },
+        ));
+      },
+      child: Container(
+        height: 85,
+        width: 165,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(category.image), fit: BoxFit.fill),
+          color: Colors.brown,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Text(
+          category.categoryName,
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
         ),
       ),
     );
